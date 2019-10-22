5440dac765b66d91f82878de97e060434bb405e4
Ece Czak <ececzak@gmail.com>
2019-10-22 11:58:29 +0200

    updated

--- intermediate/js/shop.js
+++ intermediate/js/shop.js
@@ -1,7 +1,10 @@
+window.onload = listItem();
+
 function listItem(){
-for (let i = 0; i<product.length; i++) {
+for (var i = 1; i<=product.length; i++) {
 
 	var obj = product[i];
+	
 
 //productCard:
 let productCard = document.createElement("div");
@@ -14,16 +17,16 @@ productCardImgDiv.setAttribute("class", "product-card-img");
 
 let productCardDesc = document.createElement("div");
 productCardDesc.setAttribute("class", "product-card-desc");
-productCardDesc.textContent = obj.name ;
+productCardDesc.textContent = obj.name;
 
 let productCardPrice = document.createElement("p");
-productCardDesc.setAttribute("class", "product-card-price");
-productCardDesc.textContent = obj.name +" : "+Intl.NumberFormat('de-AT', { style: 'currency', currency: 'EUR' }).format(product[i].price) ;
+productCardPrice.setAttribute("class", "product-card-price");
+productCardPrice.textContent = Intl.NumberFormat('de-AT', { style: 'currency', currency: 'EUR' }).format(obj.price) ;
 
 
 let shoppingCartButton = document.createElement("img");
 shoppingCartButton.setAttribute("class", "shopping-cart-button");
-shoppingCartButton.setAttribute("src", "./img/product/cart.png");
+shoppingCartButton.setAttribute("src", "./img/cart.png");
 
 /*shoppingCartButton.addEventListener('click', function() {
 
@@ -32,12 +35,12 @@ shoppingCartButton.setAttribute("src", "./img/product/cart.png");
   document.getElementById("product-image").src = product[i].image;
   document.getElementById("product-preis").textContent = Intl.NumberFormat('de-AT', { style: 'currency', currency: 'EUR' }).format(product[i].price);
 
-
 }
 );*/
 
 productCard.appendChild(productCardImgDiv);
 productCard.appendChild(productCardDesc);
+productCard.appendChild(productCardPrice);
 productCard.appendChild(shoppingCartButton);
 
 // elements in productCardImgDiv:
@@ -50,3 +53,4 @@ productCardImgDiv.appendChild(productCardImg);
 document.getElementById("product-area").appendChild(productCard);
 }
 }
+
